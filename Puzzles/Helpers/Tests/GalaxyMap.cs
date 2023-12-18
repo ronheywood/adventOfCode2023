4b1327925ec5d47fc741dd9dcb8782778b9e8b13
Ron Heywood <ron.heywood@spektrix.com>
2023-12-18 11:05:43 +0000

    Day 11 simple distance

---
+++ Puzzles/Helpers/Tests/GalaxyMap.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework.Constraints;
+
+namespace TestProject1.Helpers.Tests;
+
+public class GalaxyMap : PuzzleGrid
+{
+    public GalaxyMap(IEnumerable<string> mapLines) : base(mapLines)
+    {
+        
+    }
+
+    public bool RowHasGalaxy(int rowIndex)
+    {
+        return (GridCompass.AllItemsEast(rowIndex, 0).Contains("#"));
+    }
+
+    public bool ColumnHasGalaxy(int columnIndex)
+    {
+        return (GridCompass.AllItemsSouth(columnIndex, 0).Contains("#"));
+    }
+
+    public int Distance(Tuple<int, int> location1, Tuple<int, int> location2)
+    {
+        if (location1.Item1 == location2.Item1)
+        {
+            //moving along Y;
+            return location2.Item2 - location1.Item2;
+        }
+        //moving along x;
+        return location2.Item1 - location1.Item1;
+    }
+}
--- Puzzles/Helpers/Tests/GalaxyMapShould.cs
+++ Puzzles/Helpers/Tests/GalaxyMapShould.cs
@@ -30,22 +30,55 @@ public class GalaxyMapShould
 
     [TestCase(0,true)]
     [TestCase(3,false)]
-    public void Should_identify_if_row_has_no_galaxies(int rowIndex, bool shouldHaveGalaxy)
+    [TestCase(7,false)]
+    public void Should_identify_if_row_has_galaxies(int rowIndex, bool shouldHaveGalaxy)
     {
         var galaxyMap = new GalaxyMap(PuzzleInput.InputStringToArray(ExampleGalaxyMap));
         Assert.That(galaxyMap.RowHasGalaxy(rowIndex),Is.EqualTo(shouldHaveGalaxy));
-    }   
-}
-
-public class GalaxyMap : PuzzleGrid
-{
-    public GalaxyMap(IEnumerable<string> mapLines) : base(mapLines)
+    }
+    
+    [TestCase(0,true)]
+    [TestCase(2,false)]
+    [TestCase(5,false)]
+    [TestCase(8,false)]
+    public void Should_identify_if_column_has_galaxies(int columnIndex, bool shouldHaveGalaxy)
     {
-        
+        var galaxyMap = new GalaxyMap(PuzzleInput.InputStringToArray(ExampleGalaxyMap));
+        Assert.That(galaxyMap.ColumnHasGalaxy(columnIndex),Is.EqualTo(shouldHaveGalaxy));
     }
 
-    public bool RowHasGalaxy(int rowIndex)
+    [TestCase(0,0,1,0,1)]
+    [TestCase(0,0,0,1,1)]
+    [TestCase(0,0,0,2,2)]
+    [TestCase(0,0,2,0,2)]
+    [TestCase(0,4,6,4,6)]
+    public void Should_identify_distance_between_two_aligned_locations_unaffected_by_expansion(
+        int startX,
+        int startY,
+        int endX,
+        int endY,
+        int distance
+        )
     {
-        return (GridCompass.AllItemsEast(rowIndex, 0).Contains("#"));
+        var galaxyMap = new GalaxyMap(PuzzleInput.InputStringToArray(ExampleGalaxyMap));
+        var location1 = new Tuple<int, int>(startX,startY);
+        var location2 = new Tuple<int, int>(endX,endY);
+        Assert.That(galaxyMap.Distance(location1,location2),Is.EqualTo(distance));
     }
+    
+    
+    // [TestCase(0,4,6,4,6)]
+    // public void Should_identify_distance_between_two_aligned_locations_affected_by_expansion(
+    //     int startX,
+    //     int startY,
+    //     int endX,
+    //     int endY,
+    //     int distance
+    // )
+    // {
+    //     var galaxyMap = new GalaxyMap(PuzzleInput.InputStringToArray(ExampleGalaxyMap));
+    //     var location1 = new Tuple<int, int>(startX,startY);
+    //     var location2 = new Tuple<int, int>(endX,endY);
+    //     Assert.That(galaxyMap.Distance(location1,location2),Is.EqualTo(distance));
+    // }
 }
